8a11c7fa1859486037725f13b580589eb6214399
Martin Seckar <m.seckar@hotmail.com>
2021-02-18 02:16:42 +0100

    Solved L2

---
+++ Pages/Lab2_Classes/Complex.cs
@@ -0,0 +1,74 @@
+﻿using System;
+namespace OOPGitLabs.Pages.Lab2_Classes
+{
+    public class Complex
+    {
+        public double Realna;
+        public double Imaginarni;
+
+        public Complex() { }
+        public Complex(double re, double im)
+        {
+            Realna = re;
+            Imaginarni = im;
+        }
+
+        public static Complex operator +(Complex first, Complex sec)
+        {
+            return new Complex(first.Realna + sec.Realna, first.Imaginarni + sec.Imaginarni);
+        }
+
+        public static Complex operator -(Complex first, Complex sec)
+        {
+            return new Complex(first.Realna - sec.Realna, first.Imaginarni - sec.Imaginarni);
+        }
+
+        public static Complex operator *(Complex first, Complex sec)
+        {
+            return new Complex(first.Realna * sec.Realna, first.Imaginarni * sec.Imaginarni);
+        }
+
+        public static Complex operator /(Complex first, Complex sec)
+        {
+            return new Complex(first.Realna / sec.Realna, first.Imaginarni / sec.Imaginarni);
+        }
+
+        public static Boolean operator ==(Complex first, Complex sec)
+        {
+            return (first.Realna == sec.Realna && first.Imaginarni == sec.Imaginarni) ? true : false;
+        }
+
+        public static Boolean operator !=(Complex first, Complex sec)
+        {
+            return (first.Realna == sec.Realna && first.Imaginarni == sec.Imaginarni) ? false : true;
+        }
+
+        public static Complex operator -(Complex first)
+        {
+            return new Complex(-first.Realna, -first.Imaginarni);
+        }
+
+        public Complex conjugate()
+        {
+            return new Complex(this.Realna, -this.Imaginarni);
+        }
+
+        public double modus()
+        {
+            return Math.Sqrt(this.Realna * this.Realna + this.Imaginarni * this.Imaginarni);
+        }
+
+        public double argument()
+        {
+            return Math.Sqrt(this.Realna * this.Realna + this.Imaginarni * this.Imaginarni);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Realna}+{this.Imaginarni}i";
+        }
+
+    }
+
+}
+
---
+++ Pages/Lab2_Classes/TestComplex.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace OOPGitLabs.Pages.Lab2_Classes
+{
+    public class TestComplex
+    {
+        public Complex Measured;
+        public Complex Expected;
+        public string Name;
+        private const double Epsylon = 1E-6;
+
+        public static string Test(Complex me, Complex ex, string name)
+        {
+            return (Math.Abs(me.Realna - ex.Realna) < Epsylon && Math.Abs(me.Imaginarni - ex.Imaginarni) < Epsylon) ?
+                $"{name} OK" : $"{name} Chyba: Ocekavana hodnota: {ex.ToString()} Skutecna hodnota: {me.ToString()}";
+        }
+    }
+}
+
