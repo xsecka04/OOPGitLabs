3f9758d1900a998522fb9c6833b852f199944eec
Martin Seckar <m.seckar@hotmail.com>
2021-04-01 13:01:01 +0200

    Added L8 (almost)

---
+++ Pages/Lab8_Classes/ArchivTeplot.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace OOPGitLabs.Pages.Lab8_Classes
+{
+    public class ArchivTeplot
+    {
+        public ArchivTeplot()
+        {
+        }
+
+        public SortedDictionary<int, RocniTeplota> archiv = new();
+
+        public void Kalibrace(double coef)
+        {
+            foreach (KeyValuePair<int, RocniTeplota> tmp in archiv)
+            {
+                tmp.Value.MesicniTeploty = tmp.Value.MesicniTeploty.Select(s => s + coef).ToList();
+
+            }
+        }
+
+        public RocniTeplota Vyhledej(int rok)
+        {
+            return archiv.GetValueOrDefault(rok);
+        }
+
+        public string TiskTeplot()
+        {
+            string foo = "";
+            foreach (KeyValuePair<int, RocniTeplota> tmp in archiv)
+            {
+                foo += tmp.Value.ToString() + "<br>";
+            }
+            return foo;
+        }
+
+        public string TiskPrumernychRocnichTeplot()
+        {
+            string foo = "Average yearly temps: <br>";
+
+            foreach (KeyValuePair<int, RocniTeplota> tmp in archiv)
+            {
+                foo += tmp.Key + ": " + tmp.Value.PrumRocniTeplota + "<br>";
+            }
+            return foo;
+        }
+
+        public string TiskPrumernychMesicnichTeplot()
+        {
+            string foo = "Average monthly temps: ";
+
+            foreach (KeyValuePair<int, RocniTeplota> tmp in archiv)
+            {
+                //tmp.Value.MesicniTeploty.ElementAtOrDefault(1)
+            }
+            return foo;
+
+
+        }
+    }
+}
---
+++ Pages/Lab8_Classes/RocniTeplota.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OOPGitLabs.Pages.Lab8_Classes
+{
+    public class RocniTeplota
+    {
+        public RocniTeplota(int rok, List<double> val)
+        {
+            Rok = rok;
+            MesicniTeploty = val;
+        }
+
+        public int Rok;
+        public double MaxTeplota => MesicniTeploty.Max();
+        public double MinTeplota => MesicniTeploty.Min();
+        public double PrumRocniTeplota => MesicniTeploty.Average();
+
+        public List<double> MesicniTeploty;
+
+        public override string ToString()
+        {
+            return Rok + ": " + String.Join("; ", MesicniTeploty.ToArray());
+
+        }
+
+    }
+}
