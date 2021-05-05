fc023bf43ff75309dc35cbb4450f20e1b7c09dd2
Martin Seckar <m.seckar@hotmail.com>
2021-05-06 01:44:03 +0200

    Added Semester Project 12

---
+++ Pages/Project_Classes/Model.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOPGitLabs.Pages.Project_Classes
+{
+    public enum Operator
+    {
+        plus = '+',
+        minus = '-',
+        mult = '*',
+        divide = '/',
+    }
+
+    public class Model
+    {
+        public Model()
+        {
+
+        }
+
+        public List<Tuple<Complex, Operator>> numbers = new List<Tuple<Complex, Operator>>();
+
+        public string Input { get; set; }
+        public Complex Output { get; set; } = null;
+        public void Add(string input, Operator op)
+        {
+            numbers.Add(new Tuple<Complex, Operator>(Parser.Parse(input), op));
+        }
+
+        public void Ans()
+        {
+            if (Output is not null)
+            {
+                numbers = new();
+                numbers.Add(new Tuple<Complex, Operator>(Output, Operator.plus));
+                Output = null;
+            }
+        }
+
+        public void Del()
+        {
+            if (numbers.Count > 0)
+            {
+                numbers.RemoveAt(numbers.Count - 1);
+            }
+        }
+
+        public void Calculate()
+        {
+            Complex result = new(0,0);
+
+            foreach(Tuple<Complex, Operator> number in numbers)
+            {
+                switch (number.Item2) {
+
+                    case Operator.plus:
+                        result = result + number.Item1;
+                        break;
+                    case Operator.minus:
+                        result = result - number.Item1;
+                        break;
+                    case Operator.mult:
+                        result = result * number.Item1;
+                        break;
+                    case Operator.divide:
+                        result = result / number.Item1;
+                        break;
+                }
+            }
+            Output = result;
+
+        }
+    }
+
+}
---
+++ Pages/Project_Classes/Parser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace OOPGitLabs.Pages.Project_Classes
+{
+    public static class Parser
+    {
+
+        private static readonly Regex algebraic = new(@"([-+]?\d+\.?\d*|[-+]?\d*\.?\d+)\s*\+\s*([-+]?\d+\.?\d*|[-+]?\d*\.?\d+)i");
+        private static readonly Regex exponential = new(@"([-+]?\d+\.?\d*|[-+]?\d*\.?\d+)\s*e\^i\s*([-+]?\d+\.?\d*|[-+]?\d*\.?\d+)");
+
+        public static Complex Parse(string input)
+        {
+            Match match;
+            double real, img;
+            if (input.Contains("e^i"))
+            {
+                match = exponential.Match(input);
+                double r = double.Parse(match.Groups[1].Value, CultureInfo.InvariantCulture);
+                double phi = double.Parse(match.Groups[2].Value, CultureInfo.InvariantCulture);
+                real = r * Math.Cos(phi);
+                img = r * Math.Sin(phi);
+            }
+            else
+            {
+                match = algebraic.Match(input);
+                real = double.Parse(match.Groups[1].Value, CultureInfo.InvariantCulture);
+                img = double.Parse(match.Groups[2].Value, CultureInfo.InvariantCulture);
+
+            }
+            return new Complex(real, img);
+
+        }
+
+        public static string switchNotation(string input)
+        {
+            Match match = algebraic.Match(input);
+            double real = double.Parse(match.Groups[1].Value, CultureInfo.InvariantCulture);
+            double img = double.Parse(match.Groups[2].Value, CultureInfo.InvariantCulture);
+            return String.Format("{0:0.000}e^i{1:0.000}", Math.Sqrt(Math.Pow(real, 2) + Math.Pow(img, 2)), Math.Atan(img / real));
+
+        }
+    }
+}
